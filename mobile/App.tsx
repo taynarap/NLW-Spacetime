572ad784c2c1f2f47697c7334bae6c7c7a76bcf9
Taynara P <77408568+taynarap@users.noreply.github.com>
2023-05-18 18:12:31 +0100

    Aula 1 concluida

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,26 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <StatusBar style="auto" />
+    <View className='bg-gray-950 flex-1 items-center justify-center'>
+      <Text className='text-zinc-50 font-bold text-5xl'>Hello World</Text>
+      <StatusBar style="light" translucent/>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    color: '#fff',
-    fontWeight: '700',
-    fontSize: 48
-  },
-});
