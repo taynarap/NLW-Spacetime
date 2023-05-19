b00b52166428cd6724a18ec8584d006de041a8d9
Taynara P <77408568+taynarap@users.noreply.github.com>
2023-05-19 16:46:03 +0100

    Web front-end done

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { ImageBackground, Text, View } from 'react-native';
+
+{/* Import Fonts */}
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import {
+  BaiJamjuree_700Bold
+} from '@expo-google-fonts/bai-jamjuree'
+
+import blurbg from './src/assets/bg-blur.png'
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  {/* Only return info after fonts have been loaded */}
+  if(!hasLoadedFonts){
+    return null
+  }
+
   return (
-    <View className='bg-gray-950 flex-1 items-center justify-center'>
-      <Text className='text-zinc-50 font-bold text-5xl'>Hello World</Text>
+    <ImageBackground source={blurbg} 
+    className='bg-gray-900 flex-1 relative' 
+    imageStyle={{position: 'absolute', left: '-100%'}}>
+
       <StatusBar style="light" translucent/>
-    </View>
+    </ImageBackground>
   );
 }
