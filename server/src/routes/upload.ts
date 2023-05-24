f5f07e06b3be78082f100620f197cae322595adb
Taynara P <77408568+taynarap@users.noreply.github.com>
2023-05-24 14:51:00 +0100

    Upload of Images and Videos to website

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,4 +1,11 @@
+import { randomUUID } from "node:crypto";
+import { extname, resolve } from "node:path";
 import { FastifyInstance } from "fastify";
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream";
+import { promisify } from "node:util";
+
+const pump = promisify(pipeline);
 
 export async function uploadRoutes(app: FastifyInstance) {
   app.post("/upload", async (request, reply) => {
@@ -12,5 +19,28 @@ export async function uploadRoutes(app: FastifyInstance) {
     if (!upload) {
       return reply.status(400).send();
     }
+
+    // Only accept files that are images or videos
+    const mimeTypeRegex = /^(image | video) \/[a-zA-Z]+/;
+    const isValidFileFormat = mimeTypeRegex.test(upload.mimetype);
+
+    if (!isValidFileFormat) {
+      return reply.status(400).send();
+    }
+
+    // confirm if file with the same name exists before saving
+    const fileId = randomUUID();
+    const extension = extname(upload.filename);
+    const fileName = fileId.concat(extension);
+
+    const writeStream = createWriteStream(
+      resolve(__dirname, "../../uploads/", fileName)
+    );
+    await pump(upload.file, writeStream);
+
+    const fullUrl = request.protocol.concat("://").concat(request.hostname);
+    const fileUrl = new URL(`/uploads/${fileName}`, fullUrl).toString;
+
+    return { fileUrl };
   });
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -7,10 +7,16 @@ import multipart from "@fastify/multipart";
 import { memoriesRoutes } from "./routes/memories";
 import { authRoutes } from "./routes/auth";
 import { uploadRoutes } from "./routes/upload";
+import { resolve } from "node:path";
 
 const app = fastify();
 
 app.register(multipart);
+
+app.register(require("@fastify/static"), {
+  root: resolve(__dirname, "../uploads"),
+  prefix: "/uploads",
+});
 
 app.register(cors, {
   origin: true,
